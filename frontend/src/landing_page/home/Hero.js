8872bc8dc8e37acdfd34476a136a9a78debbf303
Prasadsing Solanki <prasadsingsolanki6006@gmail.com>
2026-05-07 00:43:06 +0530

    Update open account config

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -16,7 +16,7 @@ function Hero() {
                     onClick={handleSignupRedirect}
                     type='button'
                     style={{ width: '20%', margin: '0 auto' }}
-                    className="btn btn-primary rounded-md p-2 fs-5 rounded-5"
+                    className="btn signup-btn-lg btn-primary rounded-md p-2 fs-5 rounded-5"
                 >
                     Signup Now
                 </button>
